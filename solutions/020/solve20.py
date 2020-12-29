0dd746454d9ceffb1b23aba202954aac708fb827
zsarge <zackarywsargent@gmail.com>
2020-12-29 13:55:14 -0500

    Solve problem 20

---
+++ solutions/020/solve20.py
@@ -0,0 +1,16 @@
+# https://projecteuler.net/problem=20
+# Run with: 'python solve20.py'
+# using Python 3.6.9
+# by Zack Sargent
+
+# Prompt:
+
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+# Find the sum of the digits in the number 100!
+
+from math import factorial
+
+print(sum(map(int, (list(str(factorial(100)))))))
+# -> 648
