0006a81a32b3c6f673248a84145be0020fff6149
zsarge <zackarywsargent@gmail.com>
2021-02-04 09:53:19 -0500

    add '^'

--- solutions/016/solve16.py
+++ solutions/016/solve16.py
@@ -6,7 +6,7 @@
 # Prompt:
 
 # 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-# What is the sum of the digits of the number 21000?
+# What is the sum of the digits of the number 2^1000?
 
 def digits(power):
     return sum(map(int, list(str(2**power))))
