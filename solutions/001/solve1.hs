2d946fc63741577295adae3fdb141253a10b16f4
zsarge <zackarywsargent@gmail.com>
2021-04-25 15:53:48 -0400

    solve problem 1 in haskell

---
+++ solutions/001/solve1.hs
@@ -0,0 +1,14 @@
+-- https://projecteuler.net/problem=1
+-- Run with: 'ghc solve1.hs && ./solve1' or 'runhaskell solve1.hs'
+-- using Haskell with GHC 8.0.2
+-- by Zack Sargent
+
+{- Prompt:
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+-}
+
+main = print $ sum [i | i <- [0..1000-1], i `mod` 3 == 0 || i `mod` 5 == 0]
+-- -> 233168
