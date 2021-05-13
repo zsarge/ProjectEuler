1eb82b6198f6730dd5feaca183cfea923a96ee0d
zsarge <zackarywsargent@gmail.com>
2021-05-12 22:52:25 -0400

    fix formatting

--- solutions/028/solve28.java
+++ solutions/028/solve28.java
@@ -6,16 +6,16 @@
 /* Prompt:
 
 
-Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+   Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
 
-21 22 23 24 25
-20  7  8  9 10
-19  6  1  2 11
-18  5  4  3 12
-17 16 15 14 13
+   21 22 23 24 25
+   20  7  8  9 10
+   19  6  1  2 11
+   18  5  4  3 12
+   17 16 15 14 13
 
-It can be verified that the sum of the numbers on the diagonals is 101.
-What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+   It can be verified that the sum of the numbers on the diagonals is 101.
+   What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
 
 */
 
@@ -97,7 +97,7 @@ class Position {
 
 			// turn twice before increasing limit of moves
 			// thus building a spiral
-			if (shouldIncrement) 
+			if (shouldIncrement)
 				limit++;
 		}
 	}
@@ -135,12 +135,11 @@ class Spiral {
 		}
 
 		// from top right to bottom left
-		for (int x = spiral.length - 1, 
-				 y = 0; x >= 0; x--, y++) {
+		for (int x = spiral.length - 1, y = 0; x >= 0; x--, y++) {
 
 			total += spiral[y][x];
 		}
-		
+
 		// account for cross-over in center
 		// which will always equal 1
 		total--;
